6db36922fbd1e38aa271b6076dbceb31764e8acc
aajafry <aajafry2@gmail.com>
2024-03-20 12:08:24 +0600

    fix github deployment issue #02

--- app/portfolioes/[slug]/page.js
+++ app/portfolioes/[slug]/page.js
@@ -1,9 +1,20 @@
 import PortfolioTemplate from "@/components/templates/PortfolioTemplate";
+import { portfolioes } from "@/public/data/portfolioes";
+
+export function generateStaticParams() {
+  return portfolioes?.categories
+    ?.flatMap((category) => category?.items)
+    .map((item) => ({
+      slug: item.name.toLocaleLowerCase(),
+    }));
+}
 
 export default function Portfolio({ params }) {
+  const { slug } = params;
+
   return (
     <section className="content h-screen overflow-y-scroll grow px-20 py-8">
-      <PortfolioTemplate params={params.slug} />
+      <PortfolioTemplate params={slug} />
     </section>
   );
 }
